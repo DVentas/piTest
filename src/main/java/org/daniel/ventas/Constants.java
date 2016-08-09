b2e3a014fa3bc2e48093b6e5b0e27e41397e7f62
DVentas <dventas@stratio.com>
2016-08-09 08:54:53 +0200

    add constants

---
+++ src/main/java/org/daniel/ventas/Constants.java
@@ -0,0 +1,9 @@
+package org.daniel.ventas.common;
+
+public class Constants {
+
+    public static final Integer HTTP_CODE_OK = 200;
+    public static final Integer HTTP_CODE_BAD_REQUEST = 400;
+    public static final Integer HTTP_CODE_NOT_FOUND = 404;
+
+}
