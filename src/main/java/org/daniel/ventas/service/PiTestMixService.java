ab1b1145f557d5e5affc68db82a075921dd2a2d1
DanielVL <daniel.ventas.l@gmail.com>
2016-08-09 23:11:13 +0200

    implement algorithm and pass tests

--- src/main/java/org/daniel/ventas/service/PiTestMixService.java
+++ src/main/java/org/daniel/ventas/service/PiTestMixService.java
@@ -5,10 +5,11 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import org.codehaus.plexus.util.StringUtils;
+import org.daniel.ventas.model.CountCharacter;
+import org.daniel.ventas.model.CountString;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PiTestMixService {
 
@@ -80,17 +81,27 @@ public class PiTestMixService {
 
     private JsonObject processMix(final List<String> stringsToProcess) {
 
-        int indexOfString = 0;
+        int indexOfString = 1;
+
+        StringBuilder result;
 
         final Map<Integer, Map<Character, Integer>> countCharactersOfSrings =
                 new HashMap<>();
 
         try {
+
+            String strToProcessCleaner ;
+
+            Map<Character, Integer> countChars;
+
+            // Count chars for string
             for (String  strToProcess :  stringsToProcess) {
 
-                Map<Character, Integer> countChars = new HashMap<>();
+                strToProcessCleaner = strToProcess.replaceAll("[^a-z]", "");
 
-                for (Character character : strToProcess.toCharArray()) {
+                countChars = new HashMap<>();
+
+                for (Character character : strToProcessCleaner.toCharArray()) {
 
                     int actualCount = countChars.get(character) == null ? 0 : countChars.get(character);
 
@@ -101,6 +112,129 @@ public class PiTestMixService {
                 countCharactersOfSrings.put(indexOfString++, countChars);
 
             }
+
+            final TreeSet<CountString> charsOrderingWithIndex = new TreeSet<>();
+
+            CountString cs;
+            List<Integer> arrayOfIndex;
+            Map<Character, List<Integer>> mapOfCharacters;
+
+            // Order chars for all strings
+            for (Map.Entry<Integer, Map<Character, Integer>> stringMixing : countCharactersOfSrings.entrySet()) {
+
+                for (Map.Entry<Character, Integer> countCharsInMixString : stringMixing.getValue().entrySet()) {
+
+                    if (countCharsInMixString.getValue() > 1) {
+
+                        if (charsOrderingWithIndex.contains(new CountString(countCharsInMixString.getValue()))) {
+                            cs = charsOrderingWithIndex.ceiling(new CountString(countCharsInMixString.getValue()));
+
+
+                            if (cs.getCharsWithSameCountWithIndexStrings().containsKey(countCharsInMixString.getKey())) {
+                                cs.getCharsWithSameCountWithIndexStrings().get(countCharsInMixString.getKey())
+                                        .add(stringMixing.getKey());
+                            } else {
+
+                                arrayOfIndex = new ArrayList<>();
+                                arrayOfIndex.add(stringMixing.getKey());
+
+                                cs.getCharsWithSameCountWithIndexStrings().put(countCharsInMixString.getKey(), arrayOfIndex);
+                            }
+                        } else {
+
+                            arrayOfIndex = new ArrayList<>();
+                            arrayOfIndex.add(stringMixing.getKey());
+
+                            mapOfCharacters = new HashMap<>();
+                            mapOfCharacters.put(countCharsInMixString.getKey(), arrayOfIndex);
+
+                            cs = new CountString(countCharsInMixString.getValue(), mapOfCharacters);
+
+                            charsOrderingWithIndex.add(cs);
+                        }
+
+                    }
+                }
+            }
+
+            final Iterator<CountString> sumCountChars = charsOrderingWithIndex.iterator();
+
+            final Map<Character, CountCharacter> finalCountCharacters = new HashMap<>();
+
+            CountCharacter cc;
+
+            // Unique chars for all strings
+            while (sumCountChars.hasNext()) {
+
+                cs = sumCountChars.next();
+
+                for (Map.Entry<Character, List<Integer>> charListIndexes : cs.getCharsWithSameCountWithIndexStrings().entrySet()) {
+                    cc = new CountCharacter(cs.getCount(), charListIndexes.getValue());
+
+                    finalCountCharacters.put(charListIndexes.getKey(), cc);
+                }
+
+            }
+
+            final Map<Integer, Map<Character, List<Integer>>> orderedCharsByCount = new TreeMap<>(Collections.reverseOrder());
+            Map<Character, List<Integer>> finalCharsCount;
+
+            //TODO
+            // create result for any character
+            for (Map.Entry<Character, CountCharacter> uniqueCharsCount : finalCountCharacters.entrySet()) {
+
+                if (orderedCharsByCount.containsKey(uniqueCharsCount.getValue().getCount())) {
+
+                    orderedCharsByCount.get(uniqueCharsCount.getValue().getCount())
+                            .put(uniqueCharsCount.getKey(), uniqueCharsCount.getValue().getIndexes());
+
+                } else {
+
+                    finalCharsCount = new HashMap<>();
+                    finalCharsCount.put(uniqueCharsCount.getKey(), uniqueCharsCount.getValue().getIndexes());
+
+                    orderedCharsByCount.put(uniqueCharsCount.getValue().getCount(), finalCharsCount);
+                }
+            }
+
+
+            // TODO
+            // union all results
+
+            result = new StringBuilder();
+
+            for (Map.Entry<Integer, Map<Character, List<Integer>>> resultOrdered : orderedCharsByCount.entrySet()) {
+                for (Map.Entry<Character, List<Integer>> resultCharOrdered : resultOrdered.getValue().entrySet()) {
+
+                    result.append(buildString(resultOrdered.getKey(), resultCharOrdered.getKey(), resultCharOrdered.getValue(), indexOfString));
+
+                    result.append("/");
+                }
+
+            }
+
+            if (result.lastIndexOf("/") > -1) {
+                result.deleteCharAt(result.lastIndexOf("/"));
+            }
+
+
+            //for entradas en hash
+
+                // for characters in map
+                // if count > 1
+
+                // add TreeSet -> final, key : count, value hashmap<character, arrayofindex)
+
+            // for final count, hashmap
+                // for keyEntry in hashmap
+                    // if arrayofindex.size === indexOfString
+                         // add =: + character * count
+                    // else if arrayofindex.size > 1
+                        // add arrayofindex.mkString(",") : + character * count
+                    // else
+                        // add arrayofindex.toString : + character * count
+                    // Add Separator ("/")
+
         } catch (ClassCastException cce) {
             throw new RuntimeException(
                 new StringBuffer()
@@ -112,6 +246,22 @@ public class PiTestMixService {
         }
 
 
-        return new JsonObject().put("result", "22");
+        return new JsonObject().put("result", result.toString());
+    }
+
+    private String buildString (final Integer numberOfChars, final Character character, final List<Integer> indexes, final Integer numberOfStrings) {
+        final StringBuilder resultByCharAndCount = new StringBuilder();
+
+        if (indexes.size() == numberOfStrings) {
+            resultByCharAndCount.append("=");
+        } else {
+            resultByCharAndCount.append(StringUtils.join(indexes.iterator(), ","));
+        }
+
+        resultByCharAndCount.append(":");
+
+        resultByCharAndCount.append(new String(new char[numberOfChars]).replace("\0", String.valueOf(character)));
+
+        return resultByCharAndCount.toString();
     }
 }
