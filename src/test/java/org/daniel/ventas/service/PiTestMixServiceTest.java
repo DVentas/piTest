433e5307d32f5c8072a42b991259d22f3f6393ce
DanielVL <daniel.ventas.l@gmail.com>
2016-08-09 23:10:17 +0200

    add test for algoritm

--- src/test/java/org/daniel/ventas/service/PiTestMixServiceTest.java
+++ src/test/java/org/daniel/ventas/service/PiTestMixServiceTest.java
@@ -19,7 +19,40 @@ public class PiTestMixServiceTest extends PiTestMixServiceCommon {
         final String json = Json.encodePrettily(new JsonObject()
                 .put(KEY_STRINGS_TO_PROCESS, new JsonArray().add("OneElement").add("twoElement")));
 
-        this.checkResponse(context, json, "", Constants.HTTP_CODE_OK);
+        final String result = Json.encodePrettily(new JsonObject()
+                .put(Constants.KEY_RETURNED, "1:eee/2:tt/1:nn"));
+
+        this.checkResponse(context, json, result, Constants.HTTP_CODE_OK, true);
+
+    }
+
+    @Test
+    public void shouldReturnOk200WithTwoEmptyStrings(final TestContext context) {
+
+        final String json = Json.encodePrettily(new JsonObject()
+                .put(KEY_STRINGS_TO_PROCESS, new JsonArray().add("").add("")));
+
+        final String result = Json.encodePrettily(new JsonObject()
+                .put(Constants.KEY_RETURNED, ""));
+
+        this.checkResponse(context, json, result, Constants.HTTP_CODE_OK, true);
+
+    }
+
+    @Test
+    public void shouldReturnOk200WithThreeStrings(final TestContext context) {
+
+        final String json = Json.encodePrettily(new JsonObject()
+                .put(KEY_STRINGS_TO_PROCESS, new JsonArray()
+                                                    .add("firstelementiiiiiillllll")
+                                                    .add("2 12213(( ^ seeeecondAYI32  ++ - element")
+                                                    .add("third&elementoooooo")
+                                                ));
+
+        final String result = Json.encodePrettily(new JsonObject()
+                .put(Constants.KEY_RETURNED, "2:eeeeeee/1:iiiiiii/1:lllllll/3:oooooo/1,3:tt/2:nn"));
+
+        this.checkResponse(context, json, result, Constants.HTTP_CODE_OK, true);
 
     }
 
