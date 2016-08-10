0b024f4e7118eb265ad80558386daaaa22e63b15
DVentas <dventas@stratio.com>
2016-08-10 14:22:11 +0200

    remove unused code

--- src/main/java/org/daniel/ventas/model/MixResult.java
+++ src/main/java/org/daniel/ventas/model/MixResult.java
@@ -24,43 +24,17 @@ public class MixResult implements Comparable<MixResult>{
 
         return compareMix;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-
-        MixResult guest = (MixResult) obj;
-        return character == guest.getCharacter();
-    }
-
-
+    
     public Character getCharacter() {
         return character;
-    }
-
-    public void setCharacter(Character character) {
-        this.character = character;
     }
 
     public Integer getCount() {
         return count;
     }
 
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
     public List<Integer> getIndexes() {
         return indexes;
-    }
-
-    public void setIndexes(List<Integer> indexes) {
-        this.indexes = indexes;
     }
 
 }
